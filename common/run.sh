4c2aff8312cc7c03f821e3e66cd288829e796dd9
David de Amorim Cortez <dcorteztec@gmail.com>
2018-09-03 16:39:02 -0300

    Update run.sh

--- common/run.sh
+++ common/run.sh
@@ -6,4 +6,20 @@ cd ../auth-center
 
 mvn clean install
 
+cd ../edge-service
+
+mvn clean install
+
+cd ../eureka-service
+
+mvn clean install
+
+cd ../modulo-controle-coleta-definicao-cargas
+
+mvn clean install
+
+cd ../modulo-controle-frete
+
+mvn clean install
+
 exit
